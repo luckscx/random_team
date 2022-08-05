2836b486c632f085fa324428bef992852ada8cf0
Grissom <luckscx@gmail.com>
2022-08-05 10:24:26 +0800

    fix name list

---
+++ backend/add.js
@@ -0,0 +1,21 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db.spl3');
+const lodash = require("lodash")
+
+let valid_name = "lazykrisli(李镇宇);jensencheng(程玉鸿)"
+valid_name = valid_name.split(";")
+valid_name = lodash.uniq(valid_name)
+
+db.serialize(() => {
+    const stmt = db.prepare("INSERT INTO player(name,play_cnt) VALUES (?, ?)");
+    valid_name.forEach((name) => {
+        stmt.run(name,0);
+    })
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, name, play_cnt FROM player", (err, row) => {
+        console.log(row.id,row.name, row.play_cnt);
+    });
+});
+
+db.close();
