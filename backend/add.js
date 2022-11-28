6afbc91edc1a5d51abfa083858c24202a9154bc3
grissom <luckscx@gmail.com>
2022-11-28 23:01:05 +0800

    up

--- backend/add.js
+++ backend/add.js
@@ -3,7 +3,7 @@ const db = new sqlite3.Database('./db.spl3');
 const lodash = require("lodash")
 const process = require("process")
 
-let valid_name = "lazykrisli(李镇宇);jensencheng(程玉鸿)"
+let valid_name = "金炯;李书宇;季天成;邢洁琼"
 if (process.argv.length == 3)
 {
     valid_name = process.argv[2]
@@ -13,14 +13,14 @@ valid_name = valid_name.split(";")
 valid_name = lodash.uniq(valid_name)
 
 db.serialize(() => {
-    const stmt = db.prepare("INSERT INTO player(name,play_cnt) VALUES (?, ?)");
+    const stmt = db.prepare("INSERT INTO player(name,play_cnt,nickname) VALUES (?, ?, ?)");
     valid_name.forEach((name) => {
-        stmt.run(name,0);
+        stmt.run(name,0, name);
     })
     stmt.finalize();
 
-    db.each("SELECT rowid AS id, name, play_cnt FROM player", (err, row) => {
-        console.log(row.id,row.name, row.play_cnt);
+    db.each("SELECT rowid AS id, name, play_cnt, nickname, FROM player", (err, row) => {
+        console.log(row.id,row.name, row.play_cnt, row.nickname);
     });
 });
 
